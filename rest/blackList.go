aa0ff7dde0e5bcc4a1630428b38d7fab06e36fc5
Alexandru Galben <Alexandru.Galben@ness.com>
2022-05-04 13:20:38 +0300

    am facut codul mai clean

--- rest/blackList.go
+++ rest/blackList.go
@@ -28,6 +28,63 @@ type getBlacklistElements struct {
 	Response []int `json:"response"`
 }
 
+/*
+######################################################################################################
+######################################  ADD TO BLACKLIST  ############################################
+######################################################################################################
+*/
+
+func readAddBlacklistRequest(request *restful.Request) (requestQuery GetBlacklistRequest, err error) {
+
+	requestQuery.Val = new(int)
+	request.PathParameter("BlacklistParameter")
+
+	if len(request.PathParameter("BacklistParameter")) > 0 {
+		*requestQuery.Val, err = strconv.Atoi(request.PathParameter("BlacklistParameter"))
+	}
+	return
+}
+
+func (r *Service) AddBlacklistElement(request *restful.Request, response *restful.Response) {
+
+	requestQuery, err := readAddBlacklistRequest(request)
+	if err != nil {
+		buildEndPointErrorResponse(response, http.StatusBadRequest, fmt.Sprintf("error:%s", err))
+		return
+	}
+
+	r.blackList = append(r.blackList, *requestQuery.Val)
+	return
+
+}
+
+func (r *Service) printBlacklist(request *restful.Request, response *restful.Response) {
+	var responseJson getBlacklistElements
+
+	responseJson.Response = r.blackList[:]
+
+	response.WriteAsJson(responseJson)
+
+	return
+}
+
+/*
+######################################################################################################
+######################################  DELETE FROM BLACKLIST  #######################################
+######################################################################################################
+*/
+
+func readDeleteNumberRequest(request *restful.Request) (requestQuery DeleteNumberRequest, err error) {
+
+	requestQuery.Number = new(int)
+
+	if len(request.PathParameter(BlackListParameter)) > 0 {
+		*requestQuery.Number, err = strconv.Atoi(request.PathParameter(BlackListParameter))
+	}
+
+	return
+}
+
 func (r *Service) DeleteNumberRequest(request *restful.Request, response *restful.Response) {
 	var responseJson DeleteNumberResponse
 
@@ -62,69 +119,20 @@ func (r *Service) DeleteNumberRequest(request *restful.Request, response *restfu
 	response.WriteAsJson(responseJson)
 }
 
-func readAddBlacklistRequest(request *restful.Request) (requestQuery GetBlacklistRequest, err error) {
-
-	requestQuery.Val = new(int)
-	request.PathParameter("BlacklistParameter")
-
-	if len(request.PathParameter("BacklistParameter")) > 0 {
-		*requestQuery.Val, err = strconv.Atoi(request.PathParameter("BlacklistParameter"))
-	}
-	return
-}
-
-func (r *Service) AddBlacklistElement(request *restful.Request, response *restful.Response) {
-
-	requestQuery, err := readAddBlacklistRequest(request)
-	if err != nil {
-		buildEndPointErrorResponse(response, http.StatusBadRequest, fmt.Sprintf("error:%s", err))
-		return
-	}
-
-	r.blackList = append(r.blackList, *requestQuery.Val)
-	return
-
-}
-
-func readDeleteNumberRequest(request *restful.Request) (requestQuery DeleteNumberRequest, err error) {
-
-	requestQuery.Number = new(int)
-
-	if len(request.PathParameter(BlackListParameter)) > 0 {
-		*requestQuery.Number, err = strconv.Atoi(request.PathParameter(BlackListParameter))
-	}
-
-	return
-}
-
-func (r *Service) printBlacklist(request *restful.Request, response *restful.Response) {
-	var responseJson getBlacklistElements
-
-	responseJson.Response = r.blackList[:]
-
-	response.WriteAsJson(responseJson)
-
-	return
-}
-
-func duplicateInBlacklist(arr []int, el int) bool {
-	for i := 0; i < len(arr); i++ {
-		if arr[i] == el {
-			return true
-		}
-	}
-
-	return false
-}
+/*
+######################################################################################################
+###########################################  VALIDATION  #############################################
+######################################################################################################
+*/
 
 func validateDeleteNumberRequest(requestQuery DeleteNumberRequest) error {
 	if requestQuery.Number == nil {
-		return fmt.Errorf("Invalid Number: %v", *requestQuery.Number)
+		return fmt.Errorf("invalid number: %v", *requestQuery.Number)
 
 	}
 
 	if *requestQuery.Number < 0 {
-		return fmt.Errorf("Invalid Number: %v", *requestQuery.Number)
+		return fmt.Errorf("invalid number: %v", *requestQuery.Number)
 
 	}
 
@@ -134,17 +142,33 @@ func validateDeleteNumberRequest(requestQuery DeleteNumberRequest) error {
 func (r *Service) validateGetBlacklistRequest(requestQuery GetBlacklistRequest) error {
 	if requestQuery.Val == nil {
 
-		return fmt.Errorf("Invalid Number: %v", *requestQuery.Val)
+		return fmt.Errorf("invalid number: %v", *requestQuery.Val)
 	}
 
 	if *requestQuery.Val < 0 {
 
-		return fmt.Errorf("Invalid Number: %v", requestQuery.Val)
+		return fmt.Errorf("invalid number: %v", requestQuery.Val)
 	}
 
-	if duplicateInBlacklist(r.blackList, *requestQuery.Val) == true {
+	if duplicateInBlacklist(r.blackList, *requestQuery.Val) {
 		return fmt.Errorf("%v already exists!", requestQuery.Val)
 	}
 
 	return nil
+}
+
+/*
+######################################################################################################
+##################################  VERIFY DUPLICATES IN BLACKLIST ###################################
+######################################################################################################
+*/
+
+func duplicateInBlacklist(arr []int, el int) bool {
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == el {
+			return true
+		}
+	}
+
+	return false
 }
